54d0d502b9be9f9a5c1aad8a2dbb9a382d56d7b8
cesine <cesine@yahoo.com>
2015-03-20 12:55:13 +0700

    added tests based on source code

---
+++ test/views/sessions/map.js
@@ -0,0 +1,16 @@
+/* updated to be compatible with pre-1.38 databases */
+function(doc) {
+  try {
+    /* if this document has been deleted, the ignore it and return immediately */
+    if (doc.trashed && doc.trashed.indexOf("deleted") > -1) {
+      return;
+    }
+    if (doc.collection == "sessions" || (doc.sessionFields && !doc.datumFields)) {
+      var date = doc.dateModified ? doc.dateModified.replace(/["\\]/g, '') : "";
+      //doc.fieldDBtype = "Session";
+      emit(date, doc);
+    }
+  } catch (e) {
+    //emit(e, 1);
+  }
+};
